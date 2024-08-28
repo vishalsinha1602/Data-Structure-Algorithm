7ad61293bec785b4c507721beeaee678de5e3ee7
vishal_sinha <vishalsinha.inbox@gmail.com>
2024-08-28 23:03:04 +0530

    Initial commit

---
+++ cpp_revision/demo.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// First, solve the problem. Then, write the code
+
+int main()
+{
+
+    int x;
+
+    cin >> x;
+    cout << x;
+    mfnksjd return 0;
+}
